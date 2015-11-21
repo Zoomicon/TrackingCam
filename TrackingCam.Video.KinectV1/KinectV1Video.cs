937cb3d129dbf3e0f0142b843bf0bd49fc3a2bd0
George Birbilis <GitHub1@zoomicon.com>
2015-11-22 00:11:47 +0200

    - KinectV1Video plugin is now functional (serving video from a Kinect v1 sensor)

--- TrackingCam.Video.KinectV1/KinectV1Video.cs
+++ TrackingCam.Video.KinectV1/KinectV1Video.cs
@@ -1,6 +1,6 @@
 ﻿//Project: TrackingCam (http://TrackingCam.codeplex.com)
 //File: KinectV1Video.cs
-//Version: 20151117
+//Version: 20151122
 
 using System;
 using System.ComponentModel.Composition;
@@ -8,6 +8,8 @@ using System.Windows;
 
 using LightBuzz.Vitruvius;
 using LightBuzz.Vitruvius.Controls;
+using Microsoft.Kinect;
+using LightBuzz.Vitruvius.WPF;
 
 namespace TrackingCam.Plugins.Video
 {
@@ -20,7 +22,30 @@ namespace TrackingCam.Plugins.Video
 
     #region --- Fields ---
 
+    protected KinectSensor _kinectSensor;
     protected KinectViewer _kinectViewer;
+
+    #endregion
+
+    #region --- Initialization ---
+
+    public KinectV1Video()
+    {
+      _kinectSensor = SensorExtensions.Default();
+
+      if (_kinectSensor != null)
+      {
+        _kinectSensor.ColorStream.Enable();
+        _kinectSensor.ColorFrameReady += Sensor_ColorFrameReady;
+      }
+    }
+
+    private void Sensor_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+    {
+      using (var frame = e.OpenColorImageFrame())
+        if (frame != null)
+          _kinectViewer.Update(frame.ToBitmap());
+    }
 
     #endregion
 
@@ -45,18 +70,18 @@ namespace TrackingCam.Plugins.Video
       }
     }
 
-    #endregion
+#endregion
 
     #region --- Methods ---
 
     public void Start()
     {
-      //NOP
+      _kinectSensor.Start(); //note: if it takes too long to start/stop, could start ones and use Enable/Disable method on ColorStream (or even attach/detach the ColorFrameReady event handler, or use a _started class field to ignore the frame)
     }
 
     public void Stop()
     {
-      //NOP
+      _kinectSensor.Stop();
     }
 
     #endregion
